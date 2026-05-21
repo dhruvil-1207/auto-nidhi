3f79d9032dec922d9ef5295643b04cfd28b85e89
Akshara <pandyaakshara09@gmail.com>
2026-05-21 11:10:25 +0530

    backend updated

--- server/backend/database.py
+++ server/backend/database.py
@@ -1,12 +1,9 @@
 import os
-from pathlib import Path
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 from dotenv import load_dotenv
 
-env_path = Path(__file__).parent / ".env"
-load_dotenv(dotenv_path=env_path)
-
+load_dotenv()
 # Get the URL from your .env file
 DATABASE_URL = os.getenv("DATABASE_URL")
 
